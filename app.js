b12bccdc591186aeff4c2bb650ab2e58630938dd
dewebdev <dewebdev.shetty@gmail.com>
2024-06-21 00:56:42 +0530

    Added frontend and backend validations + schema validations for API header data

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const path = require("path");
 const Listing = require("./models/listing.js");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
+const wrapAysnc = require("./utils/wrapAsync.js");
+const expressError = require("./utils/expressError.js");
+const {listingSchema} = require("./schema.js");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -26,47 +29,78 @@ async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/wanderLust");
 }
 
-app.get("/", async (req, res) => {
+app.get("/", (req, res) => {
   res.render("home.ejs");
 });
 
-app.get("/listings", async (req, res) => {
-  const allListings = await Listing.find({});
-  res.render("listings.ejs", {allListings});
-});
+app.get(
+  "/listings",
+  wrapAysnc(async (req, res) => {
+    const allListings = await Listing.find({});
+    res.render("listings.ejs", {allListings});
+  })
+);
 
 app.get("/listings/new", (req, res) => {
   res.render("createList.ejs");
 });
 
-app.post("/listings/new", (req, res) => {
-  const list = new Listing(req.body.listing);
-  list.save();
-  res.redirect("/listings");
+app.post(
+  "/listings/new",
+  wrapAysnc(async (req, res, next) => {
+    let result = listingSchema.validate(req.body);
+    if (result.error) {
+      throw new expressError(400, result.error);
+    }
+    const list = new Listing(req.body.listing);
+    await list.save();
+    res.redirect("/listings");
+  })
+);
+
+app.get(
+  "/listings/:id",
+  wrapAysnc(async (req, res) => {
+    let {id} = req.params;
+    let listDetail = await Listing.findById(id);
+    res.render("showList.ejs", {listDetail});
+  })
+);
+
+app.get(
+  "/listings/:id/edit",
+  wrapAysnc(async (req, res) => {
+    let {id} = req.params;
+    let listDetail = await Listing.findById(id);
+    res.render("editList.ejs", {listDetail});
+  })
+);
+
+app.patch(
+  "/listings/:id",
+  wrapAysnc(async (req, res) => {
+    let {id} = req.params;
+    await Listing.findByIdAndUpdate(id, req.body.listing);
+    res.redirect("/listings");
+  })
+);
+
+app.delete(
+  "/listings/:id",
+  wrapAysnc(async (req, res) => {
+    let {id} = req.params;
+    await Listing.findByIdAndDelete(id);
+    res.redirect("/listings");
+  })
+);
+
+app.all("*", (req, res, next) => {
+  next(new expressError(404, "Page not found"));
 });
 
-app.get("/listings/:id", async (req, res) => {
-  let {id} = req.params;
-  let listDetail = await Listing.findById(id);
-  res.render("showList.ejs", {listDetail});
-});
-
-app.get("/listings/:id/edit", async (req, res) => {
-  let {id} = req.params;
-  let listDetail = await Listing.findById(id);
-  res.render("editList.ejs", {listDetail});
-});
-
-app.patch("/listings/:id", async (req, res) => {
-  let {id} = req.params;
-  await Listing.findByIdAndUpdate(id, req.body.listing);
-  res.redirect("/listings");
-});
-
-app.delete("/listings/:id", async (req, res) => {
-  let {id} = req.params;
-  await Listing.findByIdAndDelete(id);
-  res.redirect("/listings");
+app.use((err, req, res, next) => {
+  let {statusCode = 500, message = "Something Went wrong"} = err;
+  res.status(statusCode).render("errors.ejs", {err});
 });
 
 app.listen(PORT, (req, res) => {
